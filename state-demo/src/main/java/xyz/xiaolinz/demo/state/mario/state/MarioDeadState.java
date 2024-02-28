f4d927b70af75fe8901b77082d08951bc4bc474f
Saoforest <1051541160@qq.com>
2024-02-28 14:57:00 +0800

    feat: ✨ 补全状态模式马里奥demo的代码和uml图
    Signed-off-by: Saoforest <1051541160@qq.com>

--- state-demo/src/main/java/xyz/xiaolinz/demo/state/mario/state/MarioDeadState.java
+++ state-demo/src/main/java/xyz/xiaolinz/demo/state/mario/state/MarioDeadState.java
@@ -10,21 +10,21 @@ public class MarioDeadState implements MarioState {
 
     @Override
     public void eatMushroom(MarioStateMachine stateMachine) {
-
+        System.out.println("已经死了，吃蘑菇无效");
     }
 
     @Override
     public void eatFireFlower(MarioStateMachine stateMachine) {
-
+        System.out.println("已经死了，吃火花无效");
     }
 
     @Override
     public void enemyAttack(MarioStateMachine stateMachine) {
-
+        System.out.println("已经死了，被敌人攻击无效");
     }
 
     @Override
     public void fallIntoPit(MarioStateMachine stateMachine) {
-
+        System.out.println("已经死了，掉进坑里无效");
     }
 }
